9eeee96687297b45e24a17773dc3e798f5d755e3
brvnv <bruno.rdx@metalfan.com>
2018-10-11 09:04:04 -0300

    Quase pronto. Método para inverter pronto, comecei a verificar se o usuário quer continuar invertendo mas tive que parar ao descobrir que quando se inicializa uma variável como String jacare; e depois se atribui um valor a ela, para o Java é como se ela nem foi inicializada.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,27 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
+	static void inverte() {
+		System.out.println("Me diga algo interessante.");
+		Scanner sc=new Scanner(System.in);
+		String entrada=sc.nextLine();
+		char letraCorrente;
+		String saida;
+		
+		//Se liga nesse for
+		for(int i=entrada.length(); i==0; i--) {
+			letraCorrente=entrada.charAt(i);
+			saida=saida+letraCorrente;
+		}
+		System.out.println(saida);
+		System.out.println("De novo? s ou n");
+		String sOuN=sc.nextLine();
+		System.out.println(sOuN);
+	}
+
 public static void main (String[] args) {
-	String janaina="bbbbb";
-	System.out.println(janaina.length());
-}
+	inverte();
+	}
 }
